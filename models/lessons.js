5eded9bc421e6d14ef3e8c131f7bff970e43f8a5
gabarhdz <gabarhdz@outlook.com>
2025-08-19 20:37:41 -0600

    set   lessonSchema

--- models/lessons.js
+++ models/lessons.js
@@ -2,5 +2,7 @@ const { Schema, model, Types } = require('mongoose');
 const lessonsSchema = new Schema({
     title: {type: String, required: true},
     description: {type: String, required: true},
-    
+    user: {type: Types.ObjectId, ref: 'User', required: true},
+    exercises: [{type: Types.ObjectId, ref: 'Exercise', required: true}],
 });
+module.exports = model('Lesson', lessonsSchema);
