bea48b1eec1b689650236a6756d90a1531e2a999
gabarhdz <gabarhdz@outlook.com>
2025-08-20 08:19:45 -0600

    modify lessons model

--- models/lessons.js
+++ models/lessons.js
@@ -2,7 +2,8 @@ const { Schema, model, Types } = require('mongoose');
 const lessonsSchema = new Schema({
     title: {type: String, required: true},
     description: {type: String, required: true},
-    
+    language: {type: String, required: true},
+    level: {type: Number, required: true},
     user: {type: Types.ObjectId, ref: 'Users', required: true},
     exercises: [{type: Types.ObjectId, ref: 'Exercise', required: true}],
 });
